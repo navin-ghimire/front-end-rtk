b7a0bc7426c3c12384ba47220dd00f97da15f02e
navin-ghimire <navinghimire143@gmail.com>
2024-09-05 07:47:35 +0545

    ch

--- src/constants/api_urls.js
+++ src/constants/api_urls.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'https://back-end-server-c9oz.onrender.com';
+export const baseUrl = 'https://back-end-server-c9oz.onrender.com/api';
 export const imageUrl = 'https://back-end-server-c9oz.onrender.com';
