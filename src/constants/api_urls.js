3bb555150f9eba8c8ad7eb28de346b60a0db4b01
navin-ghimire <navinghimire143@gmail.com>
2024-09-06 13:58:10 +0545

    ok

--- src/constants/api_urls.js
+++ src/constants/api_urls.js
@@ -1,15 +1,18 @@
-export const baseUrl = 'http://localhost:5000/api';
-export const imageUrl = 'http://localhost:5000';
 
-
-
-
+// render bata live garda use garni 
 
 // export const baseUrl = 'https://back-end-server-1-28jz.onrender.com/api';
 // export const imageUrl = 'https://back-end-server-1-28jz.onrender.com';
 
 
 
+// locally live garda
 
 // export const baseUrl = 'http://192.168.1.149:5000/api';
 // export const imageUrl = 'http://192.168.1.149:5000';
+
+
+
+export const baseUrl = 'http://localhost:5000/api';
+export const imageUrl = 'http://localhost:5000';
+
